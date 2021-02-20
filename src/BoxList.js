f096ae07468706733898f44d6ea021e81b715f04
elena-vost <angara1298@gmail.com>
2021-02-20 14:30:04 +0100

    boxform component done

--- src/BoxList.js
+++ src/BoxList.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
 import Box from "./Box";
+import BoxForm from "./BoxForm";
 
 class BoxList extends Component {
   constructor(props) {
     super(props);
     this.state = { boxes: [{ width: 10, height: 40, color: "orange" }] };
+    this.create = this.create.bind(this);
+  }
+  create(newBox) {
+    this.setState({
+      boxes: [...this.state.boxes, newBox],
+    });
   }
   render() {
     const boxes = this.state.boxes.map((box) => (
@@ -13,6 +20,7 @@ class BoxList extends Component {
     return (
       <div>
         <h1>Color Box Maker Thingy</h1>
+        <BoxForm createBox={this.create} />
         {boxes}
       </div>
     );
