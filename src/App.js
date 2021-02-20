05c7b420004998c4ab3083c4d88c11dc4900692c
elena-vost <angara1298@gmail.com>
2021-02-20 13:42:03 +0100

    box body

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import Box from "./Box";
 import "./App.css";
 
 function App() {
--- src/Box.js
+++ src/Box.js
@@ -1,1 +1,9 @@
 import React, { Component } from "react";
+
+class Box extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Box;
